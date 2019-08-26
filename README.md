fc55a4294dea602197d8e26e8b50a9f506ef3914
Ronald Requena <rrequena@bcp.com.pe>
2019-08-26 02:40:44 -0500

    initial project

--- README.md
+++ README.md
@@ -14,9 +14,16 @@ Run `ng generate component component-name` to generate a new component. You can 
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
 
+- `docker-compose build` (setup environment, install dependencies)
+
 ## Running unit tests
 
 Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+Run `npm run test-headless` to execute using ChromeHeadless
+Run `npm run test-headless-docker` to execute using ChromeHeadless for docker
+
+Run `docker-compose run --rm desa npm run build` (run the whole build)
+Run `docker-compose run --rm desa npm run test-headless-docker` to execute using ChromeHeadless for docker
 
 ## Running end-to-end tests
 
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,7 +1,19 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const isDocker = require('is-docker')();
+
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
+//const ChromiumRevision = require('puppeteer/package.json').puppeteer.chromium_revision
+//const Downloader = require('puppeteer/utils/ChromiumDownloader')
+//const revisionInfo = Downloader.revisionInfo(Downloader.currentPlatform(), ChromiumRevision)
+//process.env.CHROME_BIN = revisionInfo.executablePath
+
+
 module.exports = function (config) {
+  console.log("isDocker=" + isDocker)
+  console.log("------------>" + process.env.CHROME_BIN)
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -9,15 +21,17 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-mocha-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
+    },  
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly'],
+      reports: ['html', 'lcovonly', 'text-summary', 'cobertura'],
       fixWebpackSourcePaths: true,
       thresholds: {
         statements: 80,
@@ -26,12 +40,27 @@ module.exports = function (config) {
         functions: 80
       }
     },
-    reporters: ['progress', 'kjhtml'],
+    junitReporter: {
+      outputDir: 'junit',
+      outputFile: undefined,
+      suite: '',
+      useBrowserName: false,
+      nameFormatter: undefined,
+      classNameFormatter: undefined,
+      properties: {}
+    },
+    reporters: ['coverage-istanbul', 'mocha', 'progress', 'kjhtml', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: isDocker ? ['--no-sandbox'] : []
+      }
+    },
     singleRun: false
   });
 };
